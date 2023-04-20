ecd9899ee16a47721983720156c74132d6eb655b
vali0901 <bobvalentin2003@gmail.com>
2023-04-20 20:08:15 +0300

    Idk what im doing commit

--- build/Move.d
+++
@@ -1,4 +0,0 @@
-build/Move.o: src/Move.cpp include/Move.h include/Piece.h include/Piece.h
-include/Move.h:
-include/Piece.h:
-include/Piece.h:
--- build/Table.d
+++
@@ -1,6 +0,0 @@
-build/Table.o: src/Table.cpp include/Table.h include/Piece.h \
- include/PlaySide.h include/Piece.h
-include/Table.h:
-include/Piece.h:
-include/PlaySide.h:
-include/Piece.h:
